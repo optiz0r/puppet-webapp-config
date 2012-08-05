252c1440ffa3c0bd1082bd87319dbf62f5390b33
Ben Roberts <ben@sihnon.net>
2012-08-05 17:11:57 +0100

    Allow customisation of the webapp-config install dir

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,12 +1,17 @@
 
-define webapp_config ($action='install', $vhost, $base='/', $app, $version, $depends) {
+define webapp_config ($action='install', $vhost, $base='/', $app, $version, $depends, $installdir=undef) {
+
+    $_installdir = $installdir ? {
+        undef   => "/var/www/${vhost}",
+        default => $installdir,
+    }
         
     case $action {
         "install":  { $cmd    = "webapp-config -I -h $vhost -d $base $app $version"
-                      $unless = "test -f /var/www/$vhost/htdocs/$base/.webapp-$app-$version"
+                      $unless = "test -f ${_installdir}/htdocs/$base/.webapp-$app-$version"
                     }
         "remove" :  { $cmd    = "webapp-config -C -h $vhost -d $base $app $version"
-                      $unless = "$(! test -f /var/www/$vhost/htdocs/$base/.webapp-$app-$version)"
+                      $unless = "$(! test -f ${_installdir}/htdocs/$base/.webapp-$app-$version)"
                     }
         default:    { alert( "webapp-config: Action '$action' is invalid" ) } 
     }
